9f6edd19a7ec9fe47076689f65d4ee40cc141e71
jeremyudo <98346590+jeremyudo@users.noreply.github.com>
2024-04-03 16:46:42 -0500

    still tryiong to figure out how to add book. got an add book button, a label, for now

--- app/(pages)/add_book/page.tsx
+++ app/(pages)/add_book/page.tsx
@@ -1,16 +1,45 @@
-// add_book.tsx
-import React from 'react';
+"use client"
+
+//CHECK OUT NAVIGATE STUFF
+
+import React, { useState } from 'react';
 import './add_book.css'; // Make sure the CSS file is correctly linked
 import Navbar from '@/components/Navbar';
+import Link from 'next/link';
 
 function AddBookPage() {
+  const [isbn, setIsbn] = useState('');
+  const [enteredIsbn, setEnteredIsbn] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsbn(event.target.value);
+  };
+
+  const handleAddBook = () => {
+    setEnteredIsbn(isbn);
+    setIsbn('');
+  };
+
   return (
     <div>
       <Navbar />
       <div className="addBookContent">
-        {/* Your add book page content */}
-        <h1>Add Book Page</h1>
-        <p>This is where we add books.</p> {/* Simple text */}
+        <div className="formContainer">
+          <div className="blackBar">
+            <label htmlFor="isbnInput">ISBN:</label>
+            <input 
+              type="text" 
+              id="isbnInput" 
+              value={isbn} 
+              onChange={handleInputChange} 
+            />
+          </div>
+          <button onClick={handleAddBook}>Add Book</button>
+        </div>
+        <div className="displayContainer">
+          <p>Entered ISBN:</p>
+          <p>{enteredIsbn}</p>
+        </div>
       </div>
     </div>
   );
